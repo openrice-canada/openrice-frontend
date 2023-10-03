31e75c1c046dae2eba4312eb43e7f3ab5319ecdc
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-03 16:27:17 -0400

    create login & sign up page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import SignUpPage from "./pages/sign-up/page";
+import LoginPage from "./pages/login/page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  }
 ]);
 
 export default router;
