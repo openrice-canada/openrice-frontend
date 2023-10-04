5076997955ab3b2db6a1e9f555045aca52b51b1a
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-03 22:41:24 -0400

    add error page

---
+++ src/pages/error/Error.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='h-screen flex flex-col gap-4 justify-center items-center'>
+      <img src="/error.svg" alt="error" className='max-w-lg' />
+      <h2 className='font-semibold'>Sorry, an unexpected error has occurred</h2>
+      <button type='button' className='bg-primary-600 text-[#ffffff] text-sm font-semibold px-6 py-2 rounded-full'>
+        <a href='/'>Go to home</a>
+      </button>
+    </div>
+  )
+}
+
+export default ErrorPage
