e7097d318ccab6f724efdf03e2f1afcadd4cd333
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-04 13:40:20 -0400

    update TextInput component and remove App.css

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,35 +1,54 @@
 import React from 'react'
-
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import TextInput from '../../components/Input/TextInput';
 
 function LoginPage() {
   const {
-    register,
     handleSubmit,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // formState: { errors },
-=======
     formState: { errors },
->>>>>>> parent of 2e1ab61 (eslint formatting)
-=======
-    // formState: { errors },s
->>>>>>> parent of 060c20f (refactor login/page.tsx)
+    control,
   } = useForm();
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <p className="title">Log in to Openrice</p>
+    <form
+      className='h-screen flex flex-col gap-6 justify-center max-w-sm mx-auto px-4'
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
+      <p className='text-3xl font-bold'>Log in to Openrice</p>
       <p>New to Openrice? <Link to="/sign-up">Sign-up</Link></p>
-      <br></br>
-      <input {...register('username')} placeholder="Username" className="textfield"/>
-      <br></br>
-      <br></br>
-      <input {...register('password')} placeholder="Password" className="textfield"/>
-      <br></br>
-      <br></br>
-      <input type="submit" value="Log In" className="submit"/>
+      <Controller
+        name='username'
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            label='Username'
+            type='text'
+            placeholder='Enter your username'
+            value={field.value}
+            onChange={field.onChange}  
+          />
+        )}
+      />
+      <Controller
+        name='password'
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            label='Password'
+            type='password'
+            placeholder='Enter your password'
+            value={field.value}
+            onChange={field.onChange}  
+          />
+        )}
+      />
+      <button
+        type="submit"
+        className="bg-[#000000] px-4 py-2 rounded-md text-[#ffffff] font-bold"
+      >
+        Log In
+      </button>
     </form>
   );
 }
