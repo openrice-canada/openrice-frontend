19b424e2410d71b8923cc3bd63441d358fb54a54
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-04 15:36:59 -0400

    Landing  page & add Search Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import SearchInput from './components/Input/SearchInput';
 
 function App() {
+  const { control, handleSubmit } = useForm()
   return (
     <div>
+      <form
+        className='relative h-screen flex justify-center items-center'
+        onSubmit={handleSubmit((data) => console.log(data))}
+      >
+        <img src="/hero.jpg" alt="hero" className='absolute top-0 left-0 w-full object-fill z-[-10] brightness-75' />
+        <div className='flex flex-col justify-center items-center'>
+          <h1 className='text-6xl font-bold text-[#FFFFFF] text-center drop-shadow-xl mb-12'>Discovered Restaurant <br/>& Delicious Food</h1>
+          <Controller
+            name='email'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <SearchInput
+                type='text'
+                placeholder='tacos, pizza, chinese'
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex justify-between px-4 h-16'>
+    <div className='w-full flex justify-between px-4 h-16 bg-[#FFFFFF]'>
       <a href="/" className='flex items-center gap-1'>
         <img src="/logo.png" alt="OpenRice" className='w-8 h-8' />
         <h1 className='text-2xl font-bold'>OpenRice</h1>
       </a>
+      <div className='flex items-center gap-4'>
+        <a href="/sign-up" className='text-lg font-bold'>Sign Up</a>
+        <a href="/login" className='text-lg font-bold'>Login</a>
+      </div>
     </div>
   )
 }
