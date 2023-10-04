7edc59adbfadae0be076617ab4bae01fdfc19016
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-04 17:48:03 -0400

    fix responsive in hero section

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ function App() {
         className='relative h-screen flex justify-center items-center'
         onSubmit={handleSubmit((data) => console.log(data))}
       >
-        <img src="/hero.jpg" alt="hero" className='absolute top-0 left-0 w-full object-fill z-[-10] brightness-75' />
+        <img src="/hero.jpg" alt="hero" className='absolute top-0 left-0 w-full h-screen object-cover z-[-10] brightness-75' />
         <div className='flex flex-col justify-center items-center'>
-          <h1 className='text-6xl font-bold text-[#FFFFFF] text-center drop-shadow-xl mb-12'>Discovered Restaurant <br/>& Delicious Food</h1>
+          <h1 className='md:text-6xl text-4xl font-bold text-[#FFFFFF] text-center drop-shadow-xl mb-12'>Discovered Restaurant <br/>& Delicious Food</h1>
           <Controller
             name='email'
             control={control}
