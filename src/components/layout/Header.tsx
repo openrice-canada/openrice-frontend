e69d435f42165d4b58cd806c1c334474dba2b318
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-16 16:02:31 -0400

    fix layout and image resize

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className='w-full flex justify-between px-4 h-16 bg-[#FFFFFF]'>
+    <div className='fixed top-0 left-0 w-full flex justify-between px-4 h-16 bg-[#FFFFFF]'>
       <a href="/" className='flex items-center gap-1'>
         <img src="/logo.png" alt="OpenRice" className='w-8 h-8' />
         <h1 className='text-2xl font-bold'>OpenRice</h1>
