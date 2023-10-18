6615a8c2515806a64fe1168734b01c0529a2b643
Martin Tsang <tsangmartin1225@gmail.com>
2023-10-17 21:53:58 -0400

    apply restaurant coordinate data in map

--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import MapComponent from '../../components/map/MapComponent'
+import React, { useEffect } from "react";
+import MapComponent from "../../components/map/MapComponent";
+import { getRestaurantList } from "../../api/restaurant";
+import { Restaurant } from "../../api/restaurant/type";
 
 const MapPage = () => {
-  return (
-    <MapComponent coordinates={[
-        {
-            name: 'McDonalds',
-            latitude: 43.70,
-            longitude: -79.42,
-        }
-        ]} />
-  )
-}
+  const [restaurantList, setRestaurantList] = React.useState<Restaurant[]>([]);
 
-export default MapPage
+  const fetchRestaurantList = async () => {
+    const data = await getRestaurantList();
+    setRestaurantList(data);
+  };
+
+  useEffect(() => {
+    fetchRestaurantList();
+  }, []);
+
+  return (
+    <MapComponent
+      coordinates={restaurantList
+        .filter((restaurant) => restaurant.latitude && restaurant.longitude)
+        .map((restaurant) => ({
+            name: restaurant.name,
+            latitude: parseFloat(restaurant.latitude),
+            longitude: parseFloat(restaurant.longitude),
+        }))}
+    />
+  );
+};
+
+export default MapPage;
