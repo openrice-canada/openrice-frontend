70c5f84377a13997c0dabde804df93815de52915
ttiimmothy <dinosauli2006@gmail.com>
2023-10-23 22:02:19 -0400

    Fix: update the width of textarea
    Co-authored-by: wingck <ckwwingo@gmail.com>

--- src/pages/review/ReviewPage.tsx
+++ src/pages/review/ReviewPage.tsx
@@ -1,19 +1,22 @@
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getUserList } from "../../api/user";
-import { User } from "../../api/user/UserType";
+import { useEffect } from "react";
+// import { getUserList } from "../../api/user";
+// import { User } from "../../api/user/UserType";
 
 function ReviewPage() {
   const { state } = useLocation();
-  const chosenRestaurant = state[0];
-  const [userList, setUserList] = useState<User[]>([]);
-  const fetchUserList = async () => {
-    const data = await getUserList();
-    setUserList(data);
-  };
+  let chosenRestaurant;
+  if (state) {
+    chosenRestaurant = state[0];
+  }
+  // const [userList, setUserList] = useState<User[]>([]);
+  // const fetchUserList = async () => {
+  //   const data = await getUserList();
+  //   setUserList(data);
+  // };
 
   useEffect(() => {
-    fetchUserList();
+    // fetchUserList();
   }, []);
 
   return (
@@ -26,10 +29,12 @@ function ReviewPage() {
         />
         <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black"></div>
       </div>
-      <div className="max-w-xl mx-auto mt-15">
-        <h1 className="font-bold text-4xl text-center pt-70">
-          {chosenRestaurant.name}
-        </h1>
+      <div className="max-w-5xl mx-auto mt-15">
+        {chosenRestaurant && (
+          <h1 className="font-bold text-4xl text-center pt-70">
+            {chosenRestaurant.name}
+          </h1>
+        )}
         <div>
           {chosenRestaurant && (
             <div key={chosenRestaurant.restaurantId}>
@@ -38,14 +43,16 @@ function ReviewPage() {
           )}
         </div>
         <div className="flex mt-3">
-          {userList.map((user) => (
-            <div key={user.userId}>
-              <p>{user.username}</p>
-              <div className="text-center">
-                <textarea className="border border-black px-3 py-2 rounded-md max-w-xl w-full h-40"></textarea>
-              </div>
+          {/* {userList.map((user) => ( */}
+          {/* <div key={user.userId}>
+            <p>{user.username}</p> */}
+          <div>
+            {/* <p>{user.username}</p> */}
+            <div className="text-center">
+              <textarea className="border border-black px-3 py-2 rounded-md max-w-5xl lg:w-[1024px] sm:w-[576px] h-40"></textarea>
             </div>
-          ))}
+          </div>
+          {/* ))} */}
         </div>
       </div>
     </div>
