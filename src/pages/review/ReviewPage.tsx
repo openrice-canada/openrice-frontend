e59325ed426813a308d60a29b1bc5dbd54b8e85a
ttiimmothy <dinosauli2006@gmail.com>
2023-10-25 20:53:16 -0400

    Fix: final change in the text input by deleting the max-w-5xl and changing to w-full in the uppermost div

--- src/pages/review/ReviewPage.tsx
+++ src/pages/review/ReviewPage.tsx
@@ -29,7 +29,7 @@ function ReviewPage() {
         />
         <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black"></div>
       </div>
-      <div className="max-w-5xl mx-auto mt-15">
+      <div className="w-full mx-auto mt-15">
         {chosenRestaurant && (
           <h1 className="font-bold text-4xl text-center pt-70">
             {chosenRestaurant.name}
@@ -42,14 +42,14 @@ function ReviewPage() {
             </div>
           )}
         </div>
-        <div className="flex mt-3">
+        <div className="mt-3">
           {/* {userList.map((user) => ( */}
           {/* <div key={user.userId}>
             <p>{user.username}</p> */}
           <div>
             {/* <p>{user.username}</p> */}
             <div className="text-center">
-              <textarea className="border border-black px-3 py-2 rounded-md max-w-5xl lg:w-[1024px] sm:w-[576px] h-40"></textarea>
+              <textarea className="border border-black px-3 py-2 rounded-md max-w-xl w-full h-40"></textarea>
             </div>
           </div>
           {/* ))} */}
