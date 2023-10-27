573dab0937f4a2376733170c4898a2894dc71f55
ttiimmothy <dinosauli2006@gmail.com>
2023-10-27 04:46:20 -0400

    Fix: minor change in security level by adding some strings as environmental variables

--- src/utils/imageService.ts
+++ src/utils/imageService.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  "https://wrkhhfygmoqlkgfslzza.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indya2hoZnlnbW9xbGtnZnNsenphIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjQ2NzU1MiwiZXhwIjoyMDEyMDQzNTUyfQ.djjQmGUphZV8gMr8klXaRRd2T-8DCtE-Imb9LEvAV04"
+  process.env.REACT_APP_SUPABASE_BUCKET_PATH!,
+  process.env.REACT_APP_SUPABASE_SECRET_API_KEY!
 );
 
 export async function uploadImage(file: File, restaurantId: string) {
